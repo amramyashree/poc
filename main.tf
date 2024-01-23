5369eb8aad02a9162508229d48ea7d1339e760b8
amramyashree <116065125+amramyashree@users.noreply.github.com>
2024-01-23 15:36:49 +0530

    new-commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-south-1"  # Change this to your desired region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0a7cf821b91bcccbc"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
+
