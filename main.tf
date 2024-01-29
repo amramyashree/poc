d6efd1f00718d4f8a34de3597ab03ac7d8844512
amramyashree <116065125+amramyashree@users.noreply.github.com>
2024-01-29 12:20:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "ap-south-1"  # Change this to your desired region
   assume_role {
     role_arn = "arn:aws:iam::722011624210:role/github_actions_workflow"
+    session_name = "TerraformSession"
   }
 }
 
