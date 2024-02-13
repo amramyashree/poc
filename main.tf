224ebeaddfe1a6207056fe6ea8f27c155f722592
amramyashree <116065125+amramyashree@users.noreply.github.com>
2024-02-13 13:06:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0a7cf821b91bcccbc"
+  #ami           = "ami-0a7cf821b91bcccbc"
   instance_type = "t2.micro"
 
   tags = {
