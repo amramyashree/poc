c083c24835560e3fee2cc9acc289e2cbf93a9fa8
amramyashree <116065125+amramyashree@users.noreply.github.com>
2024-02-13 17:23:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+provider "aws" {
+  region = "ap-south-1"  # Change this to your desired region
+}
 resource "aws_instance" "example" {
   ami           = "ami-0a7cf821b91bcccbc"
   instance_type = "t2.micro"
