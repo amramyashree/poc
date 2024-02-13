a491546e6bf685692c865638c10b2b4d538b4f77
amramyashree <116065125+amramyashree@users.noreply.github.com>
2024-02-13 17:13:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,32 @@
-provider "aws" {
-  region = "ap-south-1"  # Change this to your desired region
-}
-
 resource "aws_instance" "example" {
   ami           = "ami-0a7cf821b91bcccbc"
   instance_type = "t2.micro"
+  ebs_optimized = true  # Corrected to ensure EC2 is EBS optimized
 
   tags = {
     Name = "ExampleInstance2"
   }
+
+  # Adding encrypted block device configuration
+  root_block_device {
+    encrypted = true
+  }
+
+  # Adding detailed monitoring
+  monitoring = true
+
+  # Disabling instance metadata service version 1
+  metadata_options {
+    http_tokens = "required"
+  }
+
+  # IAM role attachment
+  iam_instance_profile = aws_iam_instance_profile.example.name
 }
+
+resource "aws_iam_instance_profile" "example" {
+  name = "example-instance-profile"
+  # Define the role and policies attached to this instance profile
+}
+
 
