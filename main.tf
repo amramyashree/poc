e195ebe05c37186ce46f21f35aad44fa9a5d2d4c
amramyashree <116065125+amramyashree@users.noreply.github.com>
2024-02-15 11:06:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_iam_role" "example_role" {
   # Add policies or permissions as needed
 }
 
-resource "aws_iam_instance_profile" "example_instance_profile2" {
+resource "aws_iam_instance_profile" "example_instance_profile3" {
   name = "example-instance-profile"
   role = aws_iam_role.example_role.name
 }
@@ -36,7 +36,7 @@ resource "aws_instance" "example" {
     http_tokens = "required"
   }
 
-  iam_instance_profile   = aws_iam_instance_profile.example_instance_profile2.name
+  iam_instance_profile   = aws_iam_instance_profile.example_instance_profile3.name
 
   tags = {
     Name = "ExampleInstance1"
