775b6723f82e575a03c9d9c4e81b81f1f2a9c84a
amramyashree <116065125+amramyashree@users.noreply.github.com>
2024-02-15 12:20:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,24 +21,17 @@ resource "aws_iam_instance_profile" "example_instance_profile" {
   role = aws_iam_role.example_role.name
 }
 
+provider "aws" {
+  region = "ap-south-1"  # Change this to your desired region
+}
+
 resource "aws_instance" "example" {
-  ami                    = "ami-0a7cf821b91bcccbc"
-  instance_type          = "t2.micro"
-  ebs_optimized          = true
-
-  root_block_device {
-    encrypted = true
-  }
-
-  monitoring             = true
-
-  metadata_options {
-    http_tokens = "required"
-  }
-
-  iam_instance_profile   = aws_iam_instance_profile.example_instance_profile.name
+  ami           = "ami-0a7cf821b91bcccbc"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "ExampleInstance1"
   }
+}
+
 }
