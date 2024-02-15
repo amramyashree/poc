56871217f78617f7ef79df5540b91c917b7f9b13
amramyashree <116065125+amramyashree@users.noreply.github.com>
2024-02-15 10:36:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,16 @@ provider "aws" {
   region = "ap-south-1"  # Change this to your desired region
 }
 
+resource "aws_iam_instance_profile" "example_instance_profile" {
+  name = "example-instance-profile"
+  role = aws_iam_role.example_role.name
+}
+
+resource "aws_iam_role" "example_role" {
+  name = "example-role"
+  # Add policies or permissions as needed
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0a7cf821b91bcccbc"
   instance_type = "t2.micro"
@@ -16,6 +26,8 @@ resource "aws_instance" "example" {
   metadata_options {
     http_tokens = "required"
   }
+
+  iam_instance_profile = aws_iam_instance_profile.example_instance_profile.name
 
   tags = {
     Name = "ExampleInstance2"
