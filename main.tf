971701b412e57df99d7741a0044f8e9c0a0a80f9
amramyashree <116065125+amramyashree@users.noreply.github.com>
2024-02-15 12:22:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,10 +21,6 @@ resource "aws_iam_instance_profile" "example_instance_profile" {
   role = aws_iam_role.example_role.name
 }
 
-provider "aws" {
-  region = "ap-south-1"  # Change this to your desired region
-}
-
 resource "aws_instance" "example" {
   ami           = "ami-0a7cf821b91bcccbc"
   instance_type = "t2.micro"
