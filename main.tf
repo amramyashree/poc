9b9ce7b74fce754963f936a0f03db046097cef13
amramyashree <116065125+amramyashree@users.noreply.github.com>
2024-02-16 17:38:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,45 @@
 provider "aws" {
-  region = "ap-south-1" // Specify your desired AWS region
+  region = "ap-southeast-1"  # Change this to your desired region
 }
 
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = "myyyyyyyy-bucket" // Specify your desired bucket name
-  acl    = "private" // Specify the ACL (Access Control List), in this case, private
+resource "aws_iam_role" "example_role" {
+  name               = "example-role"
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+
+  # Add policies or permissions as needed
+}
+
+resource "aws_iam_instance_profile" "example_instance_profile" {
+  name = "example-instance-profile-newname1"
+  role = aws_iam_role.example_role.name
+}
+
+resource "aws_instance" "my-instance" {
+  ami                    = "ami-03caf91bb3d81b843"
+  instance_type          = "t2.micro"
+  ebs_optimized          = true
+
+  root_block_device {
+    encrypted = true
+  }
+
+  metadata_options {
+    http_tokens = "required"
+    http_put_response_hop_limit = 2
+  }
+
+  monitoring = true
+
+  iam_instance_profile   = aws_iam_instance_profile.example_instance_profile.name
 
   tags = {
-    Name        = "ExampleBucket"
-    Environment = "Production"
+    Name = "ExampleInstance1"
   }
 }
-
