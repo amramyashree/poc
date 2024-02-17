84560f01baeedde5bb0f8008c9a3302c4f3f3c5a
amramyashree <116065125+amramyashree@users.noreply.github.com>
2024-02-17 11:55:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,56 +1,12 @@
-terraform {
-  required_version = ">= 0.12"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "aws" {
-  region = "ap-southeast-1"  # Change this to your desired region
+  region = "ap-south-1"  # Change this to your desired region
 }
 
-resource "aws_iam_role" "example_role" {
-  name               = "example-role"
-  assume_role_policy = jsonencode({
-    Version   = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = { Service = "ec2.amazonaws.com" }
-      Action    = "sts:AssumeRole"
-    }]
-  })
-
-  # Add policies or permissions as needed
-}
-
-resource "aws_iam_instance_profile" "example_instance_profile" {
-  name = "example-instance-profile-newname3"
-  role = aws_iam_role.example_role.name
-}
-
-resource "aws_instance" "my-instance" {
-  ami                    = "ami-03caf91bb3d81b843"
-  instance_type          = "t2.micro"
-  ebs_optimized          = true
-
-  root_block_device {
-    encrypted = true
-  }
-
-  metadata_options {
-    http_tokens = "required"
-    http_put_response_hop_limit = 2
-    http_endpoint = "enabled"
-  }
-
-  monitoring = true
-
-  iam_instance_profile   = aws_iam_instance_profile.example_instance_profile.name
+resource "aws_instance" "example" {
+  ami           = "ami-0a7cf821b91bcccbc"
+  instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleInstance1"
+    Name = "ExampleInstance"
   }
 }
