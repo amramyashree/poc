983efc5e58b70897ec9009a81f53ab8b1238453c
Ramya <ramyachoudary499@gmail.com>
2024-03-04 11:45:50 +0530

    new-commit-1

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "The AWS region where resources will be created."
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "ami_id" {
+  description = "The ID of the Amazon Machine Image (AMI) to use."
+  type        = string
+  default     = "ami-0a7cf821b91bcccbc"
+}
+
+variable "instance_type" {
+  description = "The EC2 instance type."
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "The name tag for the EC2 instance."
+  type        = string
+  default     = "ExampleInstance for testing atlantis...... created dummy branch..............."
+}
